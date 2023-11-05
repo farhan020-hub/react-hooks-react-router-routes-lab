e132b41fec4b0a5c3e704e1e2ae6dd1ec956557d
Saman <farhanadam394@gmail.com>
2023-11-05 18:53:45 +0300

    Completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,30 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  
+  <div>
+    
+    <h1>Movies Page</h1>
+    <div>
+      {movies.map((movie) => (
+        <div key={movie.title}>
+          <h2>{movie.title}</h2>
+          <h4>{movie.time}</h4>
+          <ul>
+            {movie.genres.map((genre) => (
+              <li key={genre}>{genre}</li>
+            ))}
+          </ul>
+          </div>
+      ))}
+    </div>
+  </div>
+   
+    );
 }
 
 export default Movies;
+// This component should render the text Movies Page in an <h1>, 
+// and make a new <div> for each movie. The <div> should contain the movie's title, 
+// time and a <ul> with a list of its genres, each within their own <li>.
